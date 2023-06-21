7a14a87fbc041247b8612582357859baa3fc7373
Sujay Mukherjee <132755939+devsujay19@users.noreply.github.com>
2023-06-22 00:12:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('This is the Apple Logo Created using HTML and CSS only.');
 
-console.log('Logo re-designed HTML and CSS Code by : Sujay Mukherjee');
+console.log('Logo re-designed in HTML and CSS Code by : Sujay Mukherjee');
 
 document.addEventListener('keydown', function(event) {
   if (event.key === 'O' && (event.ctrlKey || event.shiftKey || event.altKey)){
